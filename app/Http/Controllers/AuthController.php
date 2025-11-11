5ecf1fee603ab359b2d3412433de882a08f7f38c
dzkripcr <dzikri24si@mahasiswa.pcr.ac.id>
2025-11-11 09:52:00 +0700

    kembali ke validate awal

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,32 +24,32 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        // $username = $request->input('username');
-        // $password = $request->input('password');
+        $username = $request->input('username');
+        $password = $request->input('password');
 
-        // // Validasi: jika username dan password adalah 'nim'
-        // // if ($username === '2457301037' && $password === '2457301037') {
-        // //     return redirect('/dashboard')->with('success', 'Selamat Datang Admin!');
-        // // }
-        // // return back()->withErrors(['login_error' => 'Username atau password salah!']);
-        // return redirect('/dashboard')->with('success', 'Selamat Datang Admin!');
-        $request->validate([
-            'name'    => 'required',
-            'password' => 'required',
-        ]);
+        // Validasi: jika username dan password adalah 'nim'
+        // if ($username === '2457301037' && $password === '2457301037') {
+        //     return redirect('/dashboard')->with('success', 'Selamat Datang Admin!');
+        // }
+        // return back()->withErrors(['login_error' => 'Username atau password salah!']);
+        return redirect('/dashboard')->with('success', 'Selamat Datang Admin!');
+        // $request->validate([
+        //     'name'    => 'required',
+        //     'password' => 'required',
+        // ]);
 
-        $user = User::where('name', $request->name)->first();
+        // $user = User::where('name', $request->name)->first();
 
-        if (! $user) {
-            return redirect()->route('login')->withErrors(['name' => 'Username tidak ditemukan']);
-        }
+        // if (! $user) {
+        //     return redirect()->route('login')->withErrors(['name' => 'Username tidak ditemukan']);
+        // }
 
-        if (! Hash::check($request->password, $user->password)) {
-            return redirect()->route('login')->withErrors(['password' => 'Password salah']);
-        }
+        // if (! Hash::check($request->password, $user->password)) {
+        //     return redirect()->route('login')->withErrors(['password' => 'Password salah']);
+        // }
 
-        Auth::login($user);
-        return redirect()->route('dashboard-admin.index');
+        // Auth::login($user);
+        // return redirect()->route('dashboard-admin.index');
     }
 
     public function register(Request $request)
