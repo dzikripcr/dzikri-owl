8adad9827b555bc98433554b866dfe47cdd974a6
dzkripcr <dzikri24si@mahasiswa.pcr.ac.id>
2025-11-18 08:13:37 +0700

    penambahan filter dan search data

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -9,9 +9,11 @@ class PelangganController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data['dataPelanggan'] = Pelanggan::paginate(10)->onEachSide(2);
+        $filterableColumns = ['gender'];
+        $searchableColumns = ['first_name']; //sesuai kolom Pelanggan
+        $data['dataPelanggan'] = Pelanggan::filter($request, $filterableColumns)->search($request,$searchableColumns)->paginate(10)->withQueryString();
         return view('admin.pelanggan.index', $data);
     }
 
